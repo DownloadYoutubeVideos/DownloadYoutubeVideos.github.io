3214c528669a753b54681922104ada213254aa23
haidar786 <hussnaintarar@gmail.com>
2020-03-13 13:52:37 +0500

    initial commit

---
+++ flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "528be93c87d3f319cc9f339ec164e0ff",
+"/main.dart.js": "c83bae09cb6bf374d2e50a23f8652e6d",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/manifest.json": "6c70ae2a752f767ca6c0e4c599e922f3",
+"/assets/LICENSE": "f226e9c4ad0df436aacad6360b9ce0c2",
+"/assets/AssetManifest.json": "5c5359a38342336694ae9cc1867032f4",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/assets/qizu.gif": "81d6af46503b4b0784c1cd86cc657917"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
